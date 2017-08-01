3590b36cb3cb577ccbe2d94ca001c5b4a5aed20e
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-01 15:43:51 -0700

    Added errors middleware and created test file for band members

--- lab-eddie/server.js
+++ lab-eddie/server.js
@@ -5,11 +5,16 @@ const debug = require('debug')('app:server');
 const morgan = require('morgan');
 const creatError = require('http-errors');
 const PORT = process.env.PORT || 3000;
-
-
 const app = express();
 
+const myRouter = require('./lib/member-router.js');
+const cors = require('./lib/cors.js');
+const errors = require('./lib/errors.js');
+
 app.use(morgan('dev'));
+app.use(cors);
+app.use(myRouter);
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(console.log('Server active: ', PORT));
