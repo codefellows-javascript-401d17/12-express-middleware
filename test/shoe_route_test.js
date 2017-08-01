cb7f8c052972dcd3e1aa04df033647cc7bad4a52
Luis Manzo <manzosk3@gmail.com>
2017-08-01 15:45:43 -0700

    verifies first test (get) passes

---
+++ test/shoe_route_test.js
@@ -0,0 +1,43 @@
+const expect = require('chai').expect;
+const request = require('superagent');
+const Shoe = require('../model/shoe.js');
+
+require('../server.js');
+
+const url = 'http://localhost:8000';
+const sampleShoe = {
+  brand: 'adidas',
+  color: 'green'
+};
+
+describe('Shoe Routes', function () {
+
+  describe('/GET /api/shoe', function () {
+    describe('given a valid id', function () {
+      before((done) => {
+        Shoe.createShoe(sampleShoe)
+          .then((shoe) => {
+            this.tempShoe = shoe;
+            done();
+          })
+          .catch((err) => done(err));
+      })
+      after((done) => {
+        Shoe.deleteShoe(this.tempShoe.id)
+          .then(() => {
+            done();
+          })
+          .catch((err) => {
+            done(err);
+          })
+      })
+      it('should return a shoe', (done) => {
+        request.get(`${url}/api/shoe/${this.tempShoe.id}`)
+          .end((err, rsp) => {
+            expect(rsp.status).to.equal(200);
+            done();
+          })
+      })
+    })
+  })
+})
