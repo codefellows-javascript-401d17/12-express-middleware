a706acaa9f517ef36930653da8b4a3645c426856
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-01 13:47:04 -0700

    Added wrapper methods for bandMember model

--- lab-eddie/model/band-member.js
+++ lab-eddie/model/band-member.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const uuid = require('uuid/v4');
+const storage = require('../lib/storage');
+const debug = require('debug')('app:band-members')
 
 const BandMember = module.exports = function(first, last, ...instruments) {
+  debug('member constructor');
+  if(arguments.length < 3) return new Error('Expected at least3 arguments');
 
   this.first = first;
   this.last = last;
@@ -11,3 +15,23 @@ const BandMember = module.exports = function(first, last, ...instruments) {
 
 };
 
+BandMember.fetchMember = function(id) {
+  debug('fetchMember');
+  storage.fetchItem('bandMember', id);
+};
+
+BandMember.createMember = function(...params) {
+  debug('createMember');
+  try {
+    let member = new BandMember(...params);
+    return storage.createItem('bandMember', member);
+  } catch(err) {
+    return Promise.reject(err);
+  }
+};
+
+BandMember.deleteMember = function(id) {
+  debug('deleteMember');
+
+  storage.deleteItem('bandMember', id);
+}
