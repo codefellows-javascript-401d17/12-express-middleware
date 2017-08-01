40eb4279c2fc2b11c8829fcdee287d2e9231ffdc
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-01 15:40:02 -0700

    Wrote member routes method, created cors module

---
+++ lab-eddie/lib/member-router.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const BandMember = require('../model/band-member.js');
+const debug = require('debug');
+const memberRouter = new Router();
+
+memberRouter.post('api/band-member/:first/:last:/:instruments', jsonParser, function(req, res, next) {
+  debug('POST: api/band-member');
+
+  BandMember.createMember(...req.params)
+  .then(member => res.json(member))
+  .catch(err => next(err));
+});
+
+memberRouter.get('api/band-member/:id', function(req, res, next) {
+  debug('GET: api/band-member/:id');
+
+  BandMember.fetchMember(req.params.id)
+  .then(member => res.json(member))
+  .catch(err => next(err));
+});
+
+memberRouter.get('api/band-member', function(req, res, next) {
+  debug('GET: api/band-member/');
+
+  BandMember.fetchAll()
+  .then(band => res.json(band))
+  .catch(err => next(err));
+});
+
+memberRouter.put('api/band-member/:id/:name/:first/:last:/instruments', jsonParser, function(req, res, next){
+  debug('PUT: api/band-member');
+
+  BandMember.updateMember(...req.params)
+  .then(member = res.json(member))
+  .catch(err => next(err));
+
+});
+
+memberRouter.delete('api/band-member/:id', function(req, res, next) {
+  debug('DELETE: api/band-member/:id');
+  BandMember.deleteMember(req.params.id)
+  .then(member => res.json(member))
+  .catch(err => next(err));
+})
+
+module.exports = memberRouter;
