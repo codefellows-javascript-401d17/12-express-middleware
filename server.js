530e003a17d5589fadd58ef0148bf2be58192736
Luis Manzo <manzosk3@gmail.com>
2017-08-01 13:16:52 -0700

    sets up debug scripts

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const debug = require('debug')('shoe');
 
 app.listen(PORT, function(req, rsp) {
   console.log(`listening on port ${PORT}`);
