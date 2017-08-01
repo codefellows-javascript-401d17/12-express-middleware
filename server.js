bf89dac477f68d68f0468d84a3ab77daa796d32f
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-01 16:46:09 -0700

    finished server file

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const express = require('express');
 const createError = require('http-errors');
 const debug = require('debug')('beer:server');
 
+const beerRouter = require('.route/beer-router.js');
+const cors = require('./lib/cors-middleware.js');
+const errors = require('./lib/error-middleware.js');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(morgan('dev'));
+app.use(cors);
+app.use(beerRouter);
+app.use(errors);
 
 app.listen(PORT, () => {
   console.log(`server up ${PORT}`);
