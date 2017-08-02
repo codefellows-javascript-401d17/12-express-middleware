8feb561b6267a5ed782ce6dafb250607b38a4979
Allan Liebold <allan.liebold@gmail.com>
2017-08-01 18:36:32 -0700

    creates server and model

---
+++ route/house-router.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('house:house-router');
+const House = require('../model/house.js');
+const houseRouter = new Router();
+
+houseRouter.post('/api/house', jsonParser, function(req, res, next) {
+  debug('POST: /api/house');
+
+  House.createHouse(req.body)
+  .then(house => res.json(house))
+  .catch(err => next(err));
+});
+
+houseRouter.get('/api/house/:id', function(req, res, next) {
+  debug('GET: /api/house/:id');
+
+  House.fetchHouse(req.params.id)
+  .then(house => res.json(house))
+  .catch(err => next(err));
+});
+
+houseRouter.put('/api/house', jsonParser, function(req, res, next) {
+  debug('PUT: /api/house');
+
+  House.updateHouse(req.query.id, req.body)
+  .then(house => res.json(house))
+  .catch(err => next(err));
+});
+
+module.exports = houseRouter;
